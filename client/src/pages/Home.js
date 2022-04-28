b04bb5adb175ca515ecbe30232b799c07205ba2d
Kawan <kawanse0@gmail.com>
2022-04-29 00:20:48 +0200

    added login page and link to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -17,7 +18,28 @@ function Home() {
         </p>
       </div>
       <div className="content-center justify-center flex mt-40">
-        <button className="btn btn-wide">Launch</button>
+        <label for="my-modal-6" class="btn modal-button">
+          Launch
+        </label>
+
+        <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+        <div className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Dear user</h3>
+            <p className="py-4">
+              This is still in beta, some functions might not work properly, if
+              you found any bugs make sure to contact us as soon as possible,
+              thank you!
+            </p>
+            <div className="modal-action">
+              <Link to="/app">
+                <label for="my-modal-6" class="btn">
+                  Ok
+                </label>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
