db0b2d171c4c72afb3feeb16e0a4097e1eac3409
Kawan <kawanse0@gmail.com>
2022-04-28 23:51:59 +0200

    added home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className=" flex content-center justify-center flex-col flex-wrap font-sans">
+      <p className="text-[#FF0083] mt-10 text-lg content-center justify-center flex">
+        Echat - LIMITED BETA RELEASE
+      </p>
+      <div>
+        <p className=" text-white text-6xl mt-4">
+          The chat platform for the future
+        </p>
+      </div>
+      <div className="">
+        <p className="text-[#969696] content-center justify-center flex mt-4">
+          Echat connects you to friends, Echat's where it happens
+        </p>
+      </div>
+      <div className="content-center justify-center flex mt-40">
+        <button className="btn btn-wide">Launch</button>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
